789a03d71f11190d36232baa443f55b25c06d2f6
keiya01 <keiya.s.0210@gmail.com>
2019-05-02 14:17:44 +0900

    Add test to app.test.ts

--- src/models/__test__/app.test.ts
+++ src/models/__test__/app.test.ts
@@ -9,12 +9,24 @@ test("Change one object into one array containing two arrays", () => {
     created_at: new Date
   }
   const app = new App("Test", tableData);
-  
-  const {id, text, created_at} = tableData;
+
+  const { id, text, created_at } = tableData;
   expect(app.parseQuery()).toEqual([
     Object.keys(tableData),
     [id, text, created_at]
   ])
+});
+
+test("Get escape key such as $1 or $2 etc from table data", () => {
+  const tableData = {
+    id: 1,
+    text: "Hello",
+    created_at: new Date
+  }
+  const app = new App("Test", tableData);
+
+  expect(app.getEscapeKeys(Object.keys(tableData).length))
+    .toEqual("$1,$2,$3");
 });
 
 test("Change an array containing field and request data into a SQL function", () => {
@@ -27,7 +39,7 @@ test("Change an array containing field and request data into a SQL function", ()
 
   const app = new App(tableName, tableData);
 
-  const {id, text, created_at} = tableData;
+  const { id, text, created_at } = tableData;
   const sql = `INSERT INTO ${tableName} (${Object.keys(tableData)}) VALUES ($1,$2,$3);`;
-  expect(app.getSQL()).toEqual([sql, [id, text, created_at]]);
+  expect(app.getInsertSQL()).toEqual([sql, [id, text, created_at]]);
 });
