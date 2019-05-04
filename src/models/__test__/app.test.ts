7611c90c4e03582440e6d486f0f8741c31cd30ee
keiya01 <keiya.s.0210@gmail.com>
2019-05-04 16:28:06 +0900

    Add checking undefined function to app.test.ts

--- src/models/__test__/app.test.ts
+++ src/models/__test__/app.test.ts
@@ -14,16 +14,41 @@ test("Change one object into one array containing two arrays", () => {
   expect(app.getPrivateFunctionForTest().getFieldValue()).toEqual([id, text, created_at])
 });
 
-test("Get escape key such as $1 or $2 etc from table data", () => {
-  const tableData = {
-    id: 1,
-    text: "Hello",
-    created_at: new Date
+describe("Get escape key such as $1 or $2 etc from table data", () => {
+  type tableDataProps = {
+    id?: string,
+    text?: string,
+    created_at?: Date
   }
-  const app = new App("Test", tableData);
 
-  expect(app.getPrivateFunctionForTest().getEscapeKeys(Object.keys(tableData).length))
-    .toEqual(["$1", "$2", "$3"]);
+  const tests = [
+    {
+      description: "Get escape key from data property",
+      data: <tableDataProps>{
+        id: "1",
+        text: "test",
+        created_at: new Date
+      },
+      result: ["$1", "$2", "$3"],
+    },
+    {
+      description: "Check whether this function checks for undefined values",
+      data: {
+        id: "1",
+        text: "check undefined",
+      },
+      result: ["$1", "$2"],
+    }
+  ]
+
+  tests.map(testData => {
+    const { description, data, result } = testData;
+    test(description, () => {
+      const app = new App("Test", data);
+      expect(app.getPrivateFunctionForTest().getEscapeKeys(Object.keys(data).length))
+        .toEqual(result);
+    });
+  });
 });
 
 test("Get a template for updating data for SQL", () => {
