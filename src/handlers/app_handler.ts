1217ca7b93256a866b97cd576d322b6cb3c42538
keiya01 <keiya.s.0210@gmail.com>
2019-05-07 18:00:45 +0900

    Remove checkError function in app_handler.ts

--- src/handlers/app_handler.ts
+++ src/handlers/app_handler.ts
@@ -6,20 +6,6 @@ export interface Error {
 }
 
 export default class AppHandler {
-  public checkError(isError: boolean, messages: string[]): Error {
-    if(isError) {
-      return {
-        isError: true,
-        messages
-      }
-    }
-
-    return {
-      isError: false,
-      messages: []
-    }
-  }
-
   public validateURI(uri: string) {
     const isMatch = uri.match(/http(s)?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%+.~#?&//=]*)/g);
     if(isMatch === null) {
