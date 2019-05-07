e9c025a8f382d326effe1aed543e77e8a031b7ea
keiya01 <keiya.s.0210@gmail.com>
2019-05-07 18:01:36 +0900

    Remove error interface in app_handler.ts

--- src/handlers/app_handler.ts
+++ src/handlers/app_handler.ts
@@ -1,10 +1,5 @@
 import { UserInputError } from "apollo-server-core";
 
-export interface Error {
-    isError: boolean;
-    messages: string[];
-}
-
 export default class AppHandler {
   public validateURI(uri: string) {
     const isMatch = uri.match(/http(s)?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%+.~#?&//=]*)/g);
