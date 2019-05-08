4a97951e3d742b5ff473647a801060b085a3f550
keiya01 <keiya.s.0210@gmail.com>
2019-05-08 17:20:29 +0900

    Add comment to users_handler

--- src/handlers/users_handler.ts
+++ src/handlers/users_handler.ts
@@ -1,9 +1,10 @@
 import AppHandler from "./app_handler";
 import { UserInputError } from "apollo-server-core";
+import User from "../models/user";
 
 export default class UserHandler extends AppHandler {
-  private validate(username?: string, email?: string, password?: string) {
-    this.checkEmptyGQL({username, email, password});
+  private validate(username?: string, email?: string, token_id?: string) {
+    this.checkEmptyGQL({username, email, token_id});
 
     if(email && !this.validateEmail) {
       throw new UserInputError(`This email is invalid: ${email}`, {
@@ -11,12 +12,17 @@ export default class UserHandler extends AppHandler {
         cause: "invalid",
       });
     }
+  }
 
-    if(password && password.length < 6) {
-      throw new UserInputError("The password must be min 6 lengths", {
-        argument: "password",
-        cause: "minLength"
-      });
-    }
+  /*
+    TODO
+    Temporarily set token_id to null.
+    After building the server, set token_id to not null in postgreSQL.
+  */
+  public signup = (_, {username, email, token_id}) => {
+    this.validate(username, email, token_id);
+    
+    const u = new User({username, email, token_id, created_at: new Date()});
+    u.create();
   }
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,7 @@
 import App from "./app";
 
 export default class User extends App {
-  constructor(user?: {id?: string, username?: string, email?: string, password?: string}) {
+  constructor(user?: {id?: string, username?: string, email?: string, token_id?: string, created_at?: Date}) {
     if(!user) {
       super("accounts", {});
       return;
